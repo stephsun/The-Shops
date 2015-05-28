cfcbd72151fccf711b7b2db000dcd2ab90b319bc
Stephanie Sun <stephanie.sun@sungard.com>
2015-05-28 14:48:42 -0700

    set height to screen height

--- public/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-+function() {
-	$( document ).ready(function() {
-    	console.log('main.js');
-		$("#menu-toggle").click(function(e) {
-        	e.preventDefault();
-        	$("#wrapper").toggleClass("toggled");
-    	});
-	});
-}();
