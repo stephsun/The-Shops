8a59280adab28b453c4d33382265232ea7a026a8
ysun <yumeng.sun0103@gmail.com>
2015-06-04 22:06:07 -0700

    add brand method done

--- views/admin.js
+++ views/admin.js
@@ -26,23 +26,28 @@ var addBrand = function (req, res) {
     var longName = req.body.longName;
     var url = req.body.url;
     var name = longName.toLowerCase();
+
     q.resolve().then(function () {
         return BrandModel.addBrand(name, longName, url);
     }).then(function (brand) {
-        return BrandModel.getAllBrands();
-    }).then(function (brandList) {
-        res.render('admin', {
-            title: 'Edit',
-            brands: brandList,
-            isSuccessful: true,
-            message: 'Added sucessfully'
+        return BrandModel.getAllBrands().then(function (brandList) {
+            res.render('admin', {
+                title: 'Edit',
+                brands: brandList,
+                isSuccessful: true,
+                message: 'Added sucessfully'
+            });
         });
-    }).fail(function (err) {
-        res.render('admin', {            
-            isSuccessful: false,
-            message: 'Added failed'
+    }).fail(function () {
+        return BrandModel.getAllBrands().then(function(brandList) {
+            logger.info(brandList)
+            res.render('admin', {
+                title: 'Edit',
+                brands: brandList,
+                isSuccessful: false,
+                message: 'Added failed',
+            });
         });
-        next(err);
     });
 }
 
