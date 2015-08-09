3ecf6a38f34b87ffb6382cddcfa6611a1f954341
Stephanie <yumeng.sun0103@gmail.com>
2015-08-09 16:28:33 -0700

    Replaced index page with static html

--- views/index.js
+++ views/index.js
@@ -3,20 +3,7 @@
 var q = require('q');
 
 var renderIndexPage = function (req, res) {
-    var BrandModel = require('../models/Brand').model;
-
-    q.resolve().then(function () {
-        return BrandModel.getAllBrands();
-    }).then(function (brandList) {
-        res.render('index', {
-            title: 'J.Crew',
-            url: 'https://www.jcrew.com/index.jsp',
-            brands: brandList
-        });
-    }).fail(function (err) {
-        next(err);
-    });
-    
+    res.render('index');
 };
 
 module.exports = {
