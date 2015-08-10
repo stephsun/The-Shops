3865284d6e35b7332c6eac5ca396f9fd02213508
Stephanie <yumeng.sun0103@gmail.com>
2015-08-09 21:59:16 -0700

    Fixed index page to correctly render sidebar

--- views/index.js
+++ views/index.js
@@ -3,8 +3,20 @@
 var q = require('q');
 
 var renderIndexPage = function (req, res) {
-    res.render('index');
+	var BrandModel = require('../models/Brand').model;
+
+    q.resolve().then(function () {
+		return BrandModel.getAllBrands();
+	}).then(function (brandList) {
+		res.render('index', {
+        	title: 'Home',
+        	brands: brandList
+    	});
+	}).fail(function (err) {
+		next(err);
+	});
 };
+
 
 module.exports = {
     renderIndexPage: renderIndexPage
