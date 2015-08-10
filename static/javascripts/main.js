2856183604ed6424e3406d9d8c51df046ed19cdd
Stephanie <yumeng.sun0103@gmail.com>
2015-08-09 20:01:08 -0700

    removed tabledit

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -5,26 +5,5 @@
             $( "#wrapper" ).toggleClass("toggled");
             $( "#menu-toggle" ).find("span").toggleClass("glyphicon-chevron-right");
         });
-
-        $('#tabledit').Tabledit({
-            url: '/admin/edit',
-            hideIdentifier: true,
-            restoreButton: false,
-            columns: {
-                identifier: [0, 'id'],
-                editable: [[1, 'name'], [2, 'longName'], [3, 'url'], [4, 'rank']]
-            },
-            onSuccess: function () {
-                alert('Success');
-            },
-            onFail: function (err) {
-                alert('fail');
-                console.log(err);
-            },
-            onAlways: function (data) {
-                alert('always');
-                window.location.reload(true);
-            },
-        });
     });
 }();
