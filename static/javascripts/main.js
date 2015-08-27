a71719b0b830ba03842a777e8b619115a703df54
Stephanie <yumeng.sun0103@gmail.com>
2015-08-26 23:39:48 -0700

    Used react to rewrite toggle sidebar

--- static/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-+function() {
-    $( document ).ready(function() {
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $( "#wrapper" ).toggleClass("toggled");
-            $( "#menu-toggle" ).find("span").toggleClass("glyphicon-chevron-right");
-        });
-    });
-}();
---
+++ static/javascripts/nav.js
@@ -0,0 +1,27 @@
+var Navbar = React.createClass({
+  getInitialState: function () {
+    return { fullSize: false };
+  },
+  handleClick: function (e) {
+    if (this.state.fullSize) {
+      this.setState({ fullSize:false });
+    } else {
+      this.setState({ fullSize:true });
+    };
+    $( "#wrapper" ).toggleClass("toggled");
+  },
+  render: function () {
+      return (
+        <ul className="nav nav-tabs navbar-fixed-bottom">
+          <li>
+            <a href="#menu-toggle" className={this.state.fullSize ? "" : "hidden"} onClick={this.handleClick}><span className="glyphicon glyphicon-menu-hamburger"></span></a>
+          </li>
+          <li className="pull-right">
+            <a href="#menu-toggle" className={this.state.fullSize ? "hidden" : ""} onClick={this.handleClick}><span className="glyphicon glyphicon-resize-full"></span></a>
+          </li>
+        </ul>
+      );
+  }
+});
+
+React.render(<Navbar />, document.getElementById("navigation"));
