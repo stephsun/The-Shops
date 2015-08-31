a430bdd3bd629f9d67dc6140c3dd8863a80a5d9d
Stephanie <yumeng.sun0103@gmail.com>
2015-08-30 17:27:27 -0700

    Added nav button to admin page

--- static/javascripts/nav.js
+++ static/javascripts/nav.js
@@ -19,6 +19,9 @@ var Navbar = React.createClass({
           <li className="pull-right">
             <a href="#menu-toggle" className={this.state.fullSize ? "hidden" : ""} onClick={this.handleClick}><span className="glyphicon glyphicon-resize-full"></span></a>
           </li>
+          <li className="pull-right">
+            <a href="/admin"><span className="glyphicon glyphicon-edit"></span></a>
+          </li>
         </ul>
       );
   }
