fa8a3b72cc5ede160710034202b97d195ea3b80e
Stephanie <yumeng.sun0103@gmail.com>
2015-09-09 21:18:42 -0700

    Tempoparily remove link to admin page

--- static/javascripts/nav.js
+++ static/javascripts/nav.js
@@ -19,9 +19,6 @@ var Navbar = React.createClass({
           <li className="pull-right">
             <a href="#menu-toggle" className={this.state.fullSize ? "hidden" : ""} onClick={this.handleClick}><span className="glyphicon glyphicon-resize-full"></span></a>
           </li>
-          <li className="pull-right">
-            <a href="#"><span className="glyphicon glyphicon-edit"></span></a>
-          </li>
         </ul>
       );
   }
